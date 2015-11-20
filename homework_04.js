e6868872b8db39e88c204098724f85b3c0205ea8
lucacampara <lcampara96@gmail.com>
2015-11-20 15:12:04 +0100

    add homework_04

---
+++ homework_04.js
@@ -0,0 +1,36 @@
+
+function Stack(){
+	this.myarray = [];
+}
+
+Stack.prototype.push = function(e) { 
+    this.myarray.push(e);  
+    }
+
+Stack.prototype.pop = function() { 
+    return this.myarray.pop(); 
+    }
+
+Stack.prototype.peek = function() { 
+    return this.myarray[this.myarray.length -1] 
+    }
+
+Stack.prototype.isEmpty = function() { 
+    return this.myarray.length == 0; 
+    }
+
+
+function dec2bin(n){
+    stack = new Stack();
+
+    var result = "";
+    while(n > 0){
+        stack.push(n%2);
+        n = Math.floor(n/2);
+    }
+    while(!stack.isEmpty()){
+        result += stack.pop();
+    }
+
+    return result;
+}
